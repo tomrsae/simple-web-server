7629d5bd942f0fe2e2b9c76443f9cbebd1ac12df
Tommy René Sætre <tommy.saetre@hotmail.com>
2022-02-01 11:29:57 +0100

    Created minimal server that accepts incoming TCP requests at port 80

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream };
+
+fn main() -> std::io::Result<()> {
+    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 80);
+
+    let listener = TcpListener::bind(socket).unwrap();
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+    println!("Test!");
 }
